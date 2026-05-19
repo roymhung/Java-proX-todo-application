4a51c19c5d38f19348031447e5908bfe4e60679f
royhung <lemanhhung1555@gmail.com>
2026-05-19 17:33:51 +0700

    feat(model): create Todo entity with JPA annotations

---
+++ src/main/java/vn/proX/todoapplication/models/Todo.java
@@ -0,0 +1,55 @@
+package vn.proX.todoapplication.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private boolean isCompleted;
+
+    public Todo(Long id, String username, boolean isCompleted) {
+        this.id = id;
+        this.username = username;
+        this.isCompleted = isCompleted;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean isCompleted) {
+        this.isCompleted = isCompleted;
+    }
+
+    @Override
+    public String toString() {
+        return "Todo [id=" + id + ", username=" + username + ", isCompleted=" + isCompleted + "]";
+    }
+
+
+
+}
