37a2d29cc7579ff56383d63589532ec22c6ea02e
royhung <lemanhhung1555@gmail.com>
2026-05-19 15:56:38 +0700

    first commit

---
+++ src/test/java/vn/proX/todoapplication/ProXTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.proX.todoapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProXTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
