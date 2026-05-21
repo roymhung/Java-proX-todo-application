4c92508718bea14422ac2e9000f5290324df459a
royhung <lemanhhung1555@gmail.com>
2026-05-21 10:55:01 +0700

    feat(todo): add create todo endpoint and service logic

--- src/main/java/vn/proX/todoapplication/controller/TodoController.java
+++ src/main/java/vn/proX/todoapplication/controller/TodoController.java
@@ -2,9 +2,12 @@ package vn.proX.todoapplication.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import vn.proX.todoapplication.entity.Todo;
@@ -45,6 +48,15 @@ public class TodoController {
 
         return ResponseEntity.ok().body(listTodo);
     }
+
+    @PostMapping("/todos")
+    public ResponseEntity<Todo> createTodo(@RequestBody Todo inputTodo) {
+
+        Todo newTodo = this.todoService.handleCreateTodo(inputTodo);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(newTodo);
+    }
+
 
     @GetMapping("/update-todo")
     public String updateTodo() {
