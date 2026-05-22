1678273e0bdc6d1a57a0fb03386ddf94b9a779c2
royhung <lemanhhung1555@gmail.com>
2026-05-22 07:42:44 +0700

    feat(tests): optimal Integration Test

---
+++ src/test/java/vn/proX/todoapplication/IntegrationTest.java
@@ -0,0 +1,16 @@
+package vn.proX.todoapplication;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest()
+@ActiveProfiles("test")
+public @interface IntegrationTest {
+}
--- src/test/java/vn/proX/todoapplication/controller/UserControllerIT.java
+++ src/test/java/vn/proX/todoapplication/controller/UserControllerIT.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -11,7 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.transaction.annotation.Transactional;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -19,135 +20,140 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import vn.proX.todoapplication.IntegrationTest;
 import vn.proX.todoapplication.entity.User;
 import vn.proX.todoapplication.repository.UserRepository;
 
-@SpringBootTest
-@ActiveProfiles("test")
+@IntegrationTest
 @AutoConfigureMockMvc
+@Transactional
 public class UserControllerIT {
 
-    @Autowired
-    private MockMvc mockMvc;
+        @Autowired
+        private MockMvc mockMvc;
 
-    @Autowired
-    private UserRepository userRepository;
+        @Autowired
+        private UserRepository userRepository;
 
-    @Autowired
-    private ObjectMapper objectMapper;
+        @Autowired
+        private ObjectMapper objectMapper;
 
+        @BeforeEach
+        public void init() {
+                this.userRepository.deleteAll();
+        }
 
-    // ======== @PostMapping("/users") ========
+        // ======== @PostMapping("/users") ========
+        @Test
+        public void createUser_ShouldReturnUser_whenValid() throws Exception {
 
-    @Test
-    public void createUser_ShouldReturnUser_whenValid() throws Exception {
+                // arrange
+                User inputUser = new User(null, "John Doe", UUID.randomUUID() + "@example.com");
 
-        // arrange
-        User inputUser = new User(null, "John Doe", UUID.randomUUID() + "@example.com");
+                // act
+                String resultStr = mockMvc
+                                .perform(post("/users").contentType(MediaType.APPLICATION_JSON)
+                                                .content(objectMapper
+                                                                .writeValueAsString(inputUser)))
+                                .andExpect(status().isCreated())
+                                .andExpect(jsonPath("$.id").isNumber())
+                                .andExpect(jsonPath("$.name").value(inputUser.getName()))
+                                .andExpect(jsonPath("$.email").value(inputUser.getEmail()))
+                                .andReturn().getResponse().getContentAsString();
 
-        // act
-        String resultStr = mockMvc
-                .perform(post("/users").contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(inputUser)))
-                .andExpect(status().isCreated()).andExpect(jsonPath("$.id").isNumber())
-                .andExpect(jsonPath("$.name").value(inputUser.getName()))
-                .andExpect(jsonPath("$.email").value(inputUser.getEmail())).andReturn()
-                .getResponse().getContentAsString();
+                // assert
+                System.out.println("resultStr = " + resultStr);
+                User outputUser = objectMapper.readValue(resultStr, User.class);
 
-        // assert
-        System.out.println("resultStr = " + resultStr);
-        User outputUser = objectMapper.readValue(resultStr, User.class);
+                assertEquals(inputUser.getName(), outputUser.getName());
+                assertEquals(inputUser.getEmail(), outputUser.getEmail());
+        }
 
-        assertEquals(inputUser.getName(), outputUser.getName());
-        assertEquals(inputUser.getEmail(), outputUser.getEmail());
-    }
+        // ======== @GetMapping("/users") ========
 
-    // ======== @GetMapping("/users") ========
+        @Test
+        public void getAllUsers() throws Exception {
+                // arrange
+                User user1 = new User(null, "Alice", UUID.randomUUID() + "@example.com");
+                User user2 = new User(null, "Bob", UUID.randomUUID() + "@example.com");
 
-    @Test
-    public void getAllUsers() throws Exception {
-        // arrange
-        this.userRepository.deleteAll();
-        User user1 = new User(null, "Alice", UUID.randomUUID() + "@example.com");
-        User user2 = new User(null, "Bob", UUID.randomUUID() + "@example.com");
+                List<User> users = List.of(user1, user2);
 
-        List<User> users = List.of(user1, user2);
+                this.userRepository.saveAll(users);
 
-        this.userRepository.saveAll(users);
+                // action assert
+                String resultStr = this.mockMvc.perform(get("/users")).andExpect(status().isOk())
+                                .andReturn().getResponse().getContentAsString();
 
-        // action assert
-        String resultStr = this.mockMvc.perform(get("/users")).andExpect(status().isOk())
-                .andReturn().getResponse().getContentAsString();
+                List<User> resultUsers = objectMapper.readValue(resultStr,
+                                new TypeReference<List<User>>() {});
+                // assert
+                assertEquals(2, resultUsers.size());
+                assertEquals("Alice", resultUsers.get(0).getName());
+        }
 
-        List<User> resultUsers =
-                objectMapper.readValue(resultStr, new TypeReference<List<User>>() {});
-        // assert
-        assertEquals(2, resultUsers.size());
-        assertEquals("Alice", resultUsers.get(0).getName());
-    }
+        // ======== @GetMapping("/users/{id}") ========
+        @Test
+        public void getUserById() throws Exception {
+                // arrange
+                User user = new User(null, "Charlie", UUID.randomUUID() + "@example.com");
+                User savedUser = this.userRepository.saveAndFlush(user);
 
-    // ======== @GetMapping("/users/{id}") ========
-    @Test
-    public void getUserById() throws Exception {
-        // arrange
-        this.userRepository.deleteAll();
-        User user = new User(null, "Charlie", UUID.randomUUID() + "@example.com");
-        User savedUser = this.userRepository.saveAndFlush(user);
+                // action
+                String resultStr = this.mockMvc.perform(get("/users/{id}", savedUser.getId()))
+                                .andExpect(status().isOk()).andReturn().getResponse()
+                                .getContentAsString();
 
-        // action
-        String resultStr = this.mockMvc.perform(get("/users/{id}", savedUser.getId()))
-                .andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
+                User resultUserOutput = objectMapper.readValue(resultStr, User.class);
+                // assert
+                assertEquals(savedUser.getId(), resultUserOutput.getId());
+                assertEquals(savedUser.getName(), resultUserOutput.getName());
+        }
 
-        User resultUserOutput = objectMapper.readValue(resultStr, User.class);
-        // assert
-        assertEquals(savedUser.getId(), resultUserOutput.getId());
-        assertEquals(savedUser.getName(), resultUserOutput.getName());
-    }
+        // ======== @PutMapping("/users/{id}") ========
+        @Test
+        public void updateUser() throws Exception {
 
-    // ======== @PutMapping("/users/{id}") ========
-    @Test
-    public void updateUser() throws Exception {
+                // arrange
 
-        // arrange
-        this.userRepository.deleteAll();
+                User user = new User(null, "old-name", "old@gmail.com");
+                User userInput = this.userRepository.saveAndFlush(user);
 
-        User user = new User(null, "old-name", "old@gmail.com");
-        User userInput = this.userRepository.saveAndFlush(user);
+                User updateUser = new User(userInput.getId(), "new-name", "new@gmail.com");
 
-        User updateUser = new User(userInput.getId(), "new-name", "new@gmail.com");
+                // action
+                String resultStr = this.mockMvc
+                                .perform(put("/users/{id}", userInput.getId())
+                                                .contentType(MediaType.APPLICATION_JSON)
+                                                .content(objectMapper
+                                                                .writeValueAsBytes(updateUser)))
+                                .andExpect(status().isOk()).andReturn().getResponse()
+                                .getContentAsString();
 
-        // action
-        String resultStr = this.mockMvc
-                .perform(put("/users/{id}", userInput.getId())
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsBytes(updateUser)))
-                .andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
+                User userOutput = this.objectMapper.readValue(resultStr, User.class);
 
-        User userOutput = this.objectMapper.readValue(resultStr, User.class);
+                // assert
+                assertEquals("new-name", userOutput.getName());
+        }
 
-        // assert
-        assertEquals("new-name", userOutput.getName());
-    }
+        // ======== @DeleteMapping("/users/{id}") ========
+        @Test
+        public void deleteUser() throws Exception {
 
-    // ======== @DeleteMapping("/users/{id}") ========
-    @Test
-    public void deleteUser() throws Exception {
+                // arrange
 
-        // arrange
-        this.userRepository.deleteAll();
+                User user = new User(null, "delete-name", "delete@gmail.com");
 
-        User user = new User(null, "delete-name", "delete@gmail.com");
+                User userInput = this.userRepository.saveAndFlush(user);
 
-        User userInput = this.userRepository.saveAndFlush(user);
+                // action
+                this.mockMvc.perform(delete("/users/{id}", userInput.getId())
+                                .contentType(MediaType.APPLICATION_JSON))
+                                .andExpect(status().isNoContent());
 
-        // action
-        this.mockMvc.perform(
-                delete("/users/{id}", userInput.getId()).contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isNoContent());
+                // assert
+                long countDB = this.userRepository.count();
 
-        // assert
-        long countDB = this.userRepository.count();
-
-        assertEquals(0, countDB);
-    }
+                assertEquals(0, countDB);
+        }
 }
