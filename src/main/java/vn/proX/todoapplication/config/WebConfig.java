5c8a349253809d517feee2984c759ac5d37d026e
royhung <lemanhhung1555@gmail.com>
2026-05-24 07:31:22 +0700

    chore(config): update allowed origins for CORS configuration

--- src/main/java/vn/proX/todoapplication/config/WebConfig.java
+++ src/main/java/vn/proX/todoapplication/config/WebConfig.java
@@ -10,9 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Áp dụng cho tất cả các endpoint
-                .allowedOrigins("http://localhost:3000", "https://yourdomain.com") // Danh sách
-                                                                                   // origins được
-                                                                                   // phép
+                .allowedOrigins("http://localhost:3000", "http://localhost:5173") // Danh sách
+                // origins được
+                // phép
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Các phương thức HTTP
                                                                            // được phép
                 .allowedHeaders("*") // Cho phép tất cả headers
